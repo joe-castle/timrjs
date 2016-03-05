b9fb14da45b956f45b0bf9d4d4ce57c67a3009c4
Joe Smith <joesmith0488@gmail.com>
2016-03-05 17:24:39 +0000

    countdown and stopwatch

--- index.js
+++ index.js
@@ -3,43 +3,23 @@
 const EventEmitter = require('events');
 const errors = require('./lib/errors');
 
-// inherit from event emitter?
-
-const countdown = () => {
-  if (this.currentTime >= 0) {
-    this.ticker(this.currentTime);
-    this.currentTime -= 1;
-  } else {
-
-  }
-};
-
-const stopwatch = () => {
-  this.ticker(this.currentTime);
-  this.currentTime += 1;
-};
-
 class Timr extends EventEmitter {
   constructor(options) {
     super();
-    this.options = options;
     this.timer = null;
     this.running = false;
-    this.ticker = options.ticker;
+    this.options = options;
     this.currentTime = options.startTime;
   }
   start() {
     if (!this.running) {
       this.running = true;
-      this.timer = setInterval(() => {
-        this.ticker(this.currentTime)
-        this.options.startTime > 0 ?
-          this.currentTime -= 1
-          :
-          this.currentTime += 1
-      }, 1000);
+      this.timer = this.options.startTime > 0 ?
+        setInterval(this.countdown.bind(this), 1000)
+        :
+        setInterval(this.topwatch.bind(this), 1000);
     } else {
-      // timer already running
+      console.warn('Timer already running');
     }
     return this;
   }
@@ -57,6 +37,24 @@ class Timr extends EventEmitter {
   clear() {
     clearInterval(this.timer);
     return this;
+  }
+  ticker(fn) {
+    this.on('ticker', fn);
+  }
+  finish(fn) {
+    this.on('finish', fn);
+  }
+  stopwatch() {
+    this.emit('ticker', this.currentTime);
+    this.currentTime += 1;
+  }
+  countdown() {
+    this.emit('ticker', this.currentTime);
+    this.currentTime -= 1;
+    if (this.currentTime < 0) {
+      this.emit('finish');
+      this.stop();
+    }
   }
   getCurrentTime() {
     return this.currentTime;
@@ -83,27 +81,13 @@ const buildOptions = (options) => {
 };
 
 const timr = (options) => {
-  if (!options) {
-    throw errors.noTickerMethod;
-  }
-  if (typeof options !== 'object') {
-    throw errors.wrongOptionsType;
-  }
-  if (!options.hasOwnProperty('ticker')) {
-    throw errors.noTickerMethod;
-  }
-  if (typeof options.ticker !== 'function') {
-    throw errors.wrongTickerType;
-  }
   return new Timr(buildOptions(options));
 };
 
-const bob = timr({
-  startTime: 10,
-  ticker(currentTime) {
-    console.log(currentTime);
-  }
-});
+const bob = timr({startTime: 5});
+
+bob.ticker((time) => console.log(time));
+bob.finish(() => console.log('Timer finished'));
 
 bob.start();
 setTimeout(bob.pause.bind(bob), 2000);
