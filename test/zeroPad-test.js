87ec72c339e58de256182ef9ad5a6235e8b5c174
Joe Smith <joesmith0488@gmail.com>
2016-03-18 20:34:41 +0000

    updated zeropad function

--- test/zeroPad-test.js
+++ test/zeroPad-test.js
@@ -5,10 +5,10 @@ const expect = require('chai').expect;
 const zeroPad = require('../lib/zeroPad');
 
 describe('Zero Pad function', () => {
-  it('Adds a 0 to the beginning of the provided number is a single digit', () => {
-    expect(zeroPad(5)).to.equal('05');
+  it('Pads out single digits in a time string with a 0', () => {
+    expect(zeroPad('10:5:23')).to.equal('10:05:23');
+    expect(zeroPad('1:5:2')).to.equal('01:05:02');
+    expect(zeroPad('10:7')).to.equal('10:07');
+    expect(zeroPad('5')).to.equal('05');
   });
-  it('Returns the provided number if its 2 digits or more', () => {
-    expect(zeroPad(14)).to.equal('14');
-  })
 });
