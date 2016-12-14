f8bd3d1f770198f96af5e9f5d826b4466c3bbb2b
Joe <Joe@Joes-MacBook-Pro-2.local>
2016-12-14 18:18:14 +0000

    fixed broken link to license in banner text for dist versions

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,6 @@
 const webpack = require('webpack');
 const version = require('./package.json').version;
 
-const env = process.env.NODE_ENV;
-let bannerText = `/**
- * TimrJS v${version}
- * https://github.com/joesmith100/timrjs
- * https://www.npmjs.com/package/timrjs
- *
- * Compatible with Browsers, Node.js (CommonJS) and RequireJS.
- *
- * Copyright (c) 2016 Joe Smith
- * Released under the MIT license
- * https://github.com/joesmith100/timrjs/blob/master/LICENSE
- */`;
-
 const config = {
   output: {
     library: 'Timr',
@@ -32,7 +19,19 @@ const config = {
   },
 };
 
-if (env === 'production') {
+let bannerText = `/**
+ * TimrJS v${version}
+ * https://github.com/joesmith100/timrjs
+ * https://www.npmjs.com/package/timrjs
+ *
+ * Compatible with Browsers, Node.js (CommonJS) and RequireJS.
+ *
+ * Copyright (c) 2016 Joe Smith
+ * Released under the MIT license
+ * https://github.com/joesmith100/timrjs/blob/master/LICENSE.md
+ */`;
+
+if (process.env.NODE_ENV === 'production') {
   bannerText = `/* TimrJS v${version} | (c) 2016 Joe Smith | https://github.com/joesmith100/timrjs */`;
 
   config.plugins.push(new webpack.optimize.UglifyJsPlugin());
