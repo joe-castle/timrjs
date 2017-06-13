8e4ccbd0f1b3e4d66294cc3397821378302ed63d
Joe <joesmith0488@gmail.com>
2017-06-13 09:37:32 +0100

    creating a new timr now accepts an object to make life easier

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import createStore from './createStore';
 import zeroPad from './zeroPad';
 
 import Timr from './Timr';
+import { isObj, exists } from './validate';
 
   /**
    * @description Creates a new Timr object.
    *
-   * @param {String|Number} startTime - The starting time for the timr object.
+   * @param {String|Number|Object} startTime - The starting time for the timr object.
+   * Or an object with startTime and optinos.
    * @param {Object} [options] - Options to customise the timer.
    *
    * @return {Object} A new Timr object.
    */
 function create(startTime, options) {
+  if (isObj(startTime)) {
+    if (exists(startTime.startTime)) {
+      return new Timr(startTime.startTime, startTime);
+    }
+
+    throw new Error('When providing only an object when creating a timer, it must have a startTime property.');
+  }
+
   return new Timr(startTime, options);
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -13,13 +13,44 @@ import Timr from '../src/Timr';
 
 const requireTimr = require('../src/index');
 
-describe('Index function', () => {
+describe.only('Index function', () => {
   it('Returns a new timr object.', () => {
     expect(create('10:00')).to.be.an.instanceof(Timr);
     expect(create('10m')).to.be.an.instanceof(Timr);
     expect(create('10h')).to.be.an.instanceof(Timr);
     expect(create(600)).to.be.an.instanceof(Timr);
     expect(create(0)).to.be.an.instanceof(Timr);
+  });
+
+  it('Sets up a timr object properly if a single object is passed as the startTime', (done) => {
+    const timer = create({ startTime: 600 });
+
+    expect(timer.getStartTime()).to.equal(600);
+    expect(timer.getCurrentTime()).to.equal(600);
+
+    timer.ticker(({ formattedTime }) => {
+      expect(formattedTime).to.equal('09:59');
+      timer.destroy();
+      done();
+    }).start();
+  });
+
+  it('Throws an error if an object is passed with no / incorrect startTime value', () => {
+    expect(() => create({})).to.throw(
+      'When providing only an object when creating a timer, it must have a startTime property.',
+    );
+
+    expect(() => create({ startTime: 'invalid time' })).to.throw(
+      'Expected time to be in (hh:mm:ss) format, instead got: invalid time',
+    );
+
+    expect(() => create({ startTime: [] })).to.throw(
+      'Expected time to be a string or number, instead got: array',
+    );
+
+    expect(() => create({ startTime: -54 })).to.throw(
+      'Time cannot be a negative number, got: -54',
+    );
   });
 
   it('Exposes the top level api', () => {
